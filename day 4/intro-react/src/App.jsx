8b2cb6809d10dfe26ef6eebae33ff07107d8d2ef
Jordan <74591126+jordansmrd@users.noreply.github.com>
2024-03-27 09:22:49 +0700

    Update App.jsx

--- day 4/intro-react/src/App.jsx
+++ day 4/intro-react/src/App.jsx
@@ -2,17 +2,12 @@
 
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import BandList from "./components/band";
-import Banner from "./components/banner";
-import Navbar from "./components/navbar";
+
 import Home from "./pages/home";
 
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <Banner />
-      <BandList /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
